f098237df5e1349fa4f7895c96afc08c270dd157
lelabianca <lelabianca@loyola.edu>
2024-11-16 17:21:28 -0500

    pt 2

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
-def work_file(filename):
-    file_data = open(f'{filename}.txt', 'r')
+from itertools import count
 
+
+def work_file(filename):
+    headline_lengths = []
+    headline_word_count = []
+    headline_count = 0
+    avg_words = 0
+    avg_characters = 0
+    file_data = open(f'{filename}.txt', 'r')
+    word1 = input("\nWhat's a word you would like to search the headlines for?\nNOTE: This will not count exclusively explicit uses of the word: ")
+    word2 = input("\nWhat's another word you would like to search the headlines for?\nNOTE: This will not count exclusively explicit uses of the word: ")
+    print('\nWorking...\n')
+    for headline in file_data:
+        headline_words = []
+        word_count = 0
+        headline_lengths.append(len(headline))
+        headline = headline.split()
+        headline_words.append(headline)
+        for word in headline_words:
+            word_count += 1
+        headline_word_count.append(word_count)
+    for number in headline_word_count:
+        headline_count += 1
+        avg_words += int(number)
+        avg_words = avg_words / headline_count
+    for number in headline_lengths:
+        avg_characters += int(number)
+        avg_characters = avg_characters / headline_count
+    #print(f"\nThe word '{word1}' appears {} times.")
+    #print(f"The word '{word2}' appears {} times.")
+    print(f"The average word count of the headlines is {avg_words}.")
+    print(f"The average character count of the headlines is {avg_characters}.")
+    print(f"The shortest headline is {min(headline_word_count)} characters.")
+    print(f"The longest headline is {max(headline_word_count)} characters.\n")
+
+        
 
 def main():
     passthru = False
@@ -18,7 +52,7 @@ def main():
         while not passthru2:
             file_name = input('Enter the desired headline year in digits: ')
             if file_name.isdigit():
-                if file_name == 2014 or file_name == 2015 or file_name == 2016 or file_name == 2017 or file_name == 2018 or file_name = 2019
+                if file_name == '2014' or file_name == '2015' or file_name == '2016' or file_name == '2017' or file_name == '2018' or file_name == '2019':
                     work_file(file_name)
                     passthru2 = True
                 else:
