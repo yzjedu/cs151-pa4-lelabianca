2e24a83fd567520ef75712cd2880476725874f3c
lelabianca <lelabianca@loyola.edu>
2024-11-18 12:26:09 -0500

    pt 3

--- main.py
+++ main.py
@@ -8,33 +8,38 @@ def work_file(filename):
     avg_words = 0
     avg_characters = 0
     file_data = open(f'{filename}.txt', 'r')
-    word1 = input("\nWhat's a word you would like to search the headlines for?\nNOTE: This will not count exclusively explicit uses of the word: ")
-    word2 = input("\nWhat's another word you would like to search the headlines for?\nNOTE: This will not count exclusively explicit uses of the word: ")
+    word1 = input("\nWhat's a word you would like to search the headlines for: ")
+    word2 = input("\nWhat's another word you would like to search the headlines for: ")
     print('\nWorking...\n')
+    user_word_dict = {word1: 0, word2: 0 }
     for headline in file_data:
-        headline_words = []
         word_count = 0
-        headline_lengths.append(len(headline))
-        headline = headline.split()
-        headline_words.append(headline)
+        headline_length = len(headline)
+        headline_lengths.append(headline_length)
+        headline_words = headline.split()
         for word in headline_words:
             word_count += 1
+            if word.lower() == word1.lower():
+                user_word_dict[word1] += 1
+            elif word.lower() == word2.lower():
+                user_word_dict[word2] += 1
         headline_word_count.append(word_count)
+    total_words = 0
     for number in headline_word_count:
         headline_count += 1
-        avg_words += int(number)
-        avg_words = avg_words / headline_count
+        total_words += int(number)
+    avg_words = float(total_words / headline_count)
+    total_characters = 0
     for number in headline_lengths:
-        avg_characters += int(number)
-        avg_characters = avg_characters / headline_count
-    #print(f"\nThe word '{word1}' appears {} times.")
-    #print(f"The word '{word2}' appears {} times.")
+        total_characters += int(number)
+    avg_characters = float(total_characters / headline_count)
+    print(f"The word '{word1}' appears {user_word_dict[word1]} times.")
+    print(f"The word '{word2}' appears {user_word_dict[word2]} times.")
     print(f"The average word count of the headlines is {avg_words}.")
     print(f"The average character count of the headlines is {avg_characters}.")
-    print(f"The shortest headline is {min(headline_word_count)} characters.")
-    print(f"The longest headline is {max(headline_word_count)} characters.\n")
+    print(f"The shortest headline is {min(headline_word_count)} words and {min(headline_lengths)} characters.")
+    print(f"The longest headline is {max(headline_word_count)} words and {max(headline_lengths)} characters.\n")
 
-        
 
 def main():
     passthru = False
